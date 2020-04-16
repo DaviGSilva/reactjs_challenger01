ad33dab14b50c0755e02cba6f308bcb936192d5d
Davi Silva <davi.silva@sistemafiea.com.br>
2020-04-15 22:57:02 -0300

    CRUD no front usando axios, use state e use effect.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./styles.css";
+import api from './services/api';
+
+import './styles.css';
 
 function App() {
+  // Valor inicial, decorator set
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+  
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      likes: 0,
+      techs: [ 'PHP' ],
+      title: `Respositório ${Date.now()}`,
+      url: 'https://github.com/DaviGSilva/reactjs_challenger01'
+    });
+
+    const repos = response.data;
+
+    setRepositories([...repositories, repos]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`/repositories/${id}`, {});
+
+    if (response.status === 204)
+    setRepositories(
+      repositories.filter(repos => repos.id !== id)
+    );
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {
+          repositories.map(repos =>
+            <li key={repos.id}>
+              {repos.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <button onClick={() => handleRemoveRepository(repos.id)}>Remover</button>
+            </li>
+          )
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
